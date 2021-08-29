bae8482c81b73df905e61cbda739b07692ad31ca
Wellington Santos <wellsantosprofissional.dev@gmail.com>
2021-08-28 22:15:54 -0300

    Primeiro commit

---
+++ javascript-para-iniciantes/iniciante/typeof.js
@@ -0,0 +1,21 @@
+// Declare uma variável contendo uma string
+let city = 'São Sebastião';
+
+// Declare uma variável contendo um número dentro de uma string
+let year = '2021'
+
+// Declare uma variável com a sua idade
+let age = 19;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+let name = 'Wellington';
+let surname = 'Matheus';
+let soma = name + surname;
+console.log(soma)
+
+// Coloque a seguinte frase em uma variável: It's time
+let time = "It's time"
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof name)
---
+++ javascript-para-iniciantes/iniciante/variaveis.js
@@ -0,0 +1,15 @@
+// Declarar uma variável com seu nome:
+let name = 'Wellington';
+
+// Declarar uma váriavel com sua idade:
+let age = 19;
+
+// Declarar uma váriavel com a sua comiga favorita
+// e não atribuir valor
+let comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Pizza";
+
+// Declarar 5 variáveis diferentes sem valores
+let time, cidade, familia, graduacao, hobbie;
