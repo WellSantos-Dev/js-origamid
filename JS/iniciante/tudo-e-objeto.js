816448237a64532e249b2a5bd45588452c19ce31
Wellington Santos <wellsantosprofissional.dev@gmail.com>
2021-09-01 12:52:20 -0300

    Estudos do dia 01/09/21

---
+++ JS/iniciante/tudo-e-objeto.js
@@ -0,0 +1,14 @@
+// nomeie 3 propriedades ou métodos de strings
+let nome = "Wellington";
+// toLowerCase
+// toUpperCase
+// length
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+let btn = document.querySelector('.btn');
+// addEventListener
+// appendChild
+// getAttribute
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
