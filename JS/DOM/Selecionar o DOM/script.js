a2a33d30761693b1bf1c37e42a031d5cb3c424a5
Wellington Santos <wellsantosprofissional.dev@gmail.com>
2021-09-05 10:25:14 -0300

    Backup para trocar S.O

--- JS/DOM/Selecionar o DOM/script.js
+++ JS/DOM/Selecionar o DOM/script.js
@@ -1,17 +1,19 @@
 // Seleciona pelo ID
-const animais = document.getElementById('animais');
-console.log(animais);
+let animais = document.getElementById('animais');
+// Seleciona por classe
+const gridSection = document.getElementsByClassName('grid-section');
+// Seleciona por tag
+const contato = document.getElementsByTagName('ul');
 
-let pessoa = {
-  estaComendo: false,
-  nome: "Wellington",
-  falar() {
-    if (this.estaComendo === true) {
-      console.log(`O ${this.nome} não pode falar pois está comendo.`);
-    } else if (this.estaComendo === false) {
-      console.log(`O ${this.nome} está falando`);
-    }
-  }
-}
 
-pessoa.falar()
+// Selecionando por tag
+const ul = document.getElementsByTagName('ul');
+console.log(ul);
+
+// ^ (começa com)
+const linkInterno = document.querySelector('[href^="#"]');
+console.log(linkInterno);
+
+// Selectiona tudo
+const img = document.querySelectorAll('.animais img');
+console.log(img)
