c4fbefe8c55a338abb4ebaf0d8898def9b99009b
Well Santos <wellsantosprofissional.dev@gmail.com>
2021-09-13 19:51:19 -0300

    Aprendendo DOM

--- JS/DOM/Selecionar o DOM/script.js
+++ JS/DOM/Selecionar o DOM/script.js
@@ -1,6 +1,14 @@
-// Selecionando tudo pelo ID
+// Seleciona pela ID
 let animais = document.getElementById('animais');
 console.log(animais)
 
-let contato = document.getElementById('contato');
+// Seleciona pela classe, retorna uma HTMLCollection
+let gridSection = document.getElementsByClassName('grid-section');
+console.log(gridSection)
+let contato = document.getElementsByClassName('grid-section contato');
 console.log(contato)
+
+
+// Retorna todas as UL's, retorna uma HTMLCollection
+const ul = document.getElementsByTagName('ul');
+console.log(ul[1].innerHTML)
