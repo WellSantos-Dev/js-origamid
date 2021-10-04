80ac2fd6e981a3ff578ea4620932dadea3bd715b
Well Santos <wellsantosprofissional.dev@gmail.com>
2021-10-04 05:17:03 -0300

    Recaptulando DOM

--- JS/DOM/0305-classes-e-atributos/script.js
+++ JS/DOM/0305-classes-e-atributos/script.js
@@ -13,3 +13,26 @@ if(menu.classList.contains('ola')) {
 }
 
 console.log(menu.classList[2])
+
+const menu = document.querySelector('.menu');
+menu.classList.add('ativo', 'teste')
+menu.classList.remove('azul')
+menu.classList.toggle('azul')
+
+if(menu.classList.contains('azul')) {
+  menu.classList.add('possui-azul')
+} else {
+  console.log("Não possui azul")
+}
+
+console.log(menu.classList);
+
+
+
+
+const animais = document.querySelector('.animais')
+animais.setAttribute('src', 'https://www.google.com')
+animais.setAttribute('alt', 'É uma raposa')
+let existeAlt = animais.hasAttribute('alt');
+console.log(existeAlt)
+console.log(animais.attributes[4].value)
--- JS/DOM/0308-traversing-e-manipulacao/script.js
+++ JS/DOM/0308-traversing-e-manipulacao/script.js
@@ -1,32 +1,32 @@
 
-// lista.parentElement; // Pai
-// lista.parentElement.parentElement; // Pai do pai
-// lista.previousElementSibling // Elemento acima
-// lista.nextElementSibling // Elemento abaixo
+lista.parentElement; // Pai
+lista.parentElement.parentElement; // Pai do pai
+lista.previousElementSibling // Elemento acima
+lista.nextElementSibling // Elemento abaixo
 
-// const animais = document.querySelector('.animais')
-// const lista = document.querySelector('.animais-lista')
-// const contato = document.querySelector('.contato')
-// const titulo = contato.querySelector('.titulo')
-// const mapa = document.querySelector('.mapa')
+const animais = document.querySelector('.animais')
+const lista = document.querySelector('.animais-lista')
+const contato = document.querySelector('.contato')
+const titulo = contato.querySelector('.titulo')
+const mapa = document.querySelector('.mapa')
 
-// lista.appendChild(titulo) // appendChild move par ao filho do conteudo
-// animais.appendChild(contato) 
+lista.appendChild(titulo) // appendChild move par ao filho do conteudo
+animais.appendChild(contato) 
 
-// contato.insertBefore(animais, mapa) // insere animais antes de mapa
-// contato.removeChild(titulo) // Remove titulo de contato
-// contato.replaceChild(lista, titulo) // Substitui a lista pelo titulo
+contato.insertBefore(animais, mapa) // insere animais antes de mapa
+contato.removeChild(titulo) // Remove titulo de contato
+contato.replaceChild(lista, titulo) // Substitui a lista pelo titulo
 
-// const novoh1 = document.createElement('h1')
-// novoh1.innerText = "Ola, amigo"
-// novoh1.classList.add('titulo')
+const novoh1 = document.createElement('h1')
+novoh1.innerText = "Ola, amigo"
+novoh1.classList.add('titulo')
 
-// contato.appendChild(novoh1)
+contato.appendChild(novoh1)
 
-// const h1 = document.querySelector('h1')
-// cloneh1 = h1.cloneNode(true)
-// cloneh1.classList.add('azul')
-// mapa.appendChild(cloneh1)
+const h1 = document.querySelector('h1')
+cloneh1 = h1.cloneNode(true)
+cloneh1.classList.add('azul')
+mapa.appendChild(cloneh1)
 
 
 // EXERCICIOS
